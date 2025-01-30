bcf73b1eecce106bdd4ea2d1a58977f272293089
8949897221 <vanshyadav2808@gmail.com>
2025-01-30 10:26:44 +0530

    add pandas DataFrame to display student data including names, registration numbers, cities, and percentage marks

---
+++ LPU_Colab_files/panda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data={'Name':['Vansh','Piyush','Rahul','Rohit','Rajat'],
+      'Reg No.':[245,311,421,508,601],
+      'City':['Ludhiana','Maga','Chandgarh','Delhi','Jalandhar'],
+      'Percentage Marks':[75,80,85,60,95]
+}
+df=pd.DataFrame(data)
+print(df)
+print(df['Reg No.'])
