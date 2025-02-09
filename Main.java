18566e5b39986e7ccdeae0ae82e585b58ec43766
8949897221 <vanshyadav2808@gmail.com>
2025-02-09 12:43:17 +0530

    add NumberContainers class to manage index-number mappings and provide retrieval functionality

--- Main.java
+++ Main.java
@@ -268,6 +268,46 @@ public class Main{
         System.out.println(rev);
     }
 }
+import java.util.*;
+
+class NumberContainers {
+    private Map<Integer, Integer> indexToNumber; // Maps index -> number
+    private Map<Integer, TreeSet<Integer>> numberToIndices; // Maps number -> sorted indices
+
+    public NumberContainers() {
+        indexToNumber = new HashMap<>();
+        numberToIndices = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        if (indexToNumber.containsKey(index)) {
+            int oldNumber = indexToNumber.get(index);
+            if (oldNumber != number) {
+                numberToIndices.get(oldNumber).remove(index);
+                if (numberToIndices.get(oldNumber).isEmpty()) {
+                    numberToIndices.remove(oldNumber);
+                }
+            }
+        }
+        indexToNumber.put(index, number);
+        numberToIndices.computeIfAbsent(number, k -> new TreeSet<>()).add(index);
+    }
+
+    public int find(int number) {
+        if (numberToIndices.containsKey(number) && !numberToIndices.get(number).isEmpty()) {
+            return numberToIndices.get(number).first(); // Smallest index
+        }
+        return -1;
+    }
+}
+
+/**
+ * Example usage:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index, number);
+ * int result = obj.find(number);
+ */
+
 
 
 
