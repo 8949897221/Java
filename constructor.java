a977a415010b6736e967edf733c228f9fa4c5c13
8949897221 <vanshyadav2808@gmail.com>
2025-02-10 14:42:10 +0530

    refactor constructor class to update main method and simplify output

--- constructor.java
+++ constructor.java
@@ -1,14 +1,25 @@
+// public class constructor{
+//     static int a=6;
+//     static void lpu(){
+//         System.out.println("I am in lpu");
+//     }
+//     static void lpu(int x){
+//         System.out.println(x);
+//     }
+//     public static void main(String arg[]){
+//         lpu();
+//         lpu(5);
+//         System.out.println(a);
+//     }
+// }
+
+
 public class constructor{
-    static int a=6;
-    static void lpu(){
-        System.out.println("I am in lpu");
-    }
-    static void lpu(int x){
-        System.out.println(x);
-    }
-    public static void main(String arg[]){
-        lpu();
-        lpu(5);
-        System.out.println(a);
-    }
+   {
+       System.out.println("LPU");
+   }
+   public static void main(String arg[]){
+       System.out.println("Jalandhar");
+       Main obj=new Main();
+   }
 }
