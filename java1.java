b54cd683cec6fb06da5cfe1f38a320565add25b0
8949897221 <vanshyadav2808@gmail.com>
2025-02-12 21:32:00 +0530

    fix duplicate Main class definition and streamline main method

--- java1.java
+++ java1.java
@@ -7,3 +7,12 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}public class Vansh
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
