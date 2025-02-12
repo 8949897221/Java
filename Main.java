be44a9148931f5b878e282d7e8e57bebed1a0a53
8949897221 <vanshyadav2808@gmail.com>
2025-02-12 21:38:36 +0530

    remove commented-out code for Scanner and 2D array initialization in Main class

--- Main.java
+++ Main.java
@@ -1,4 +1,28 @@
 // You are using Java
+// import java.util.*;
+// public class Main{
+//     public static void main(String arg[]){
+//         Scanner sc=new Scanner(System.in);
+//         int [][] a=new int[3][];
+//         a[0]=new int [2];
+//         a[1]=new int [2];
+//         a[2]=new int [4];
+//         System.out.println(a.length);
+//         System.out.println(a[1].length);
+//         for(int i=0;i<a.length;i++){
+//             for(int j=0;j<a[i].length;j++){
+//                 a[i][j]=sc.nextInt();
+//             }
+//         }
+//          for(int i=0;i<a.length;i++){
+//             for(int j=0;j<a[i].length;j++){
+//                 System.out.print(a[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 import java.util.*;
 public class Main{
     public static void main(String arg[]){
