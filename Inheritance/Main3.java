1e6cb98489415d2986607372057fcbd92f3204b9
8949897221 <vanshyadav2808@gmail.com>
2025-02-22 18:43:20 +0530

    refactor: remove class B and its instances from Main3 to simplify inheritance demonstration

--- Inheritance/Main3.java
+++ Inheritance/Main3.java
@@ -6,17 +6,3 @@ class A{
         System.out.println(x+2);
     }
 }
-class B extends A{
-    B(){
-        System.out.println("child");
-    }
-    B(int y){
-        System.out.println(y+3);
-    }
-}
-public class Main3{
-    public static void main(String[] args){
-        new B();
-        new B(5);
-    }
-}
