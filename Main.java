91afd7966813e31fdb8fbdf2b1a2645e75b3b71e
8949897221 <vanshyadav2808@gmail.com>
2025-02-24 15:36:15 +0530

    refactor: update Main class to demonstrate method overloading with lpu methods

--- Main.java
+++ Main.java
@@ -1,20 +1,35 @@
-// You are using Java
-class A{
-    final void lpu(){
-        System.out.println("abc");
+// // You are using Java
+// class A{
+//     final void lpu(){
+//         System.out.println("abc");
         
-        //Note: final method cannot override 
-    }
-}
-class B extends A{
-    void lpu(){
-        super.lpu();
-        System.out.println("xyz");
-    }
-}
+//         //Note: final method cannot override 
+//     }
+// }
+// class B extends A{
+//     void lpu(){
+//         super.lpu();
+//         System.out.println("xyz");
+//     }
+// }
+// public class Main{
+//     public static void main(String arg[]){
+//         B obj=new B();
+//         obj.lpu();
+//     }
+// }
+
+// You are using Java
 public class Main{
+    void lpu(int x){
+        System.out.println(x);
+    }
+    void lpu(int x,int y){
+        System.out.println(x+y);
+    }
     public static void main(String arg[]){
-        B obj=new B();
-        obj.lpu();
+        Main obj=new Main();
+        obj.lpu(5);
+        obj.lpu(5,3);
     }
 }
